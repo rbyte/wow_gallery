86eee3b66e2c3e075f826db328d56a69e5a3a8b7
matmax <matthias.graf@st.ovgu.de>
2017-02-23 17:57:10 +0100

    used php to simplify

---
+++ download.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$allowedDirs = array("screens/full/", "screens/300x225/", "panos/16x10_2560_q90/", "panos/FULL_q70/");
+	$dir = $allowedDirs[0];
+	foreach ($allowedDirs as $val) {
+		if (isset($_GET[$val]))
+			$dir = $val;
+	}
+	
+	if ($handle = opendir($dir)) {
+		while (false !== ($file = readdir($handle))) {
+			if ($file != "." && $file != "..") {
+				echo "\n<img src='".$dir.$file."'>";
+			}
+		}
+		closedir($handle);
+	}
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>World of Warcraft Wallpapers by storke: High Definition Gallery</title>
+	<meta name="description" content="World of Warcraft Wallpapers by storke: High Definition Gallery">
+	<meta name="author" content="storke">
+	<meta name="keywords" content="eclasca, storke, world of warcraft, wow, wallpaper, wallpapers, high definition, hd, panorama, lothar" lang="en">
+	<link rel="shortcut icon" href="assets/favicon.png">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+<div id="topShadow"></div>
+
+<header>
+	<ul>
+
+<li>About
+	<div><div>
+	<p><img src="ava/FearTheMindFlayer.jpg" class="imgInText">Welcome to my World of Warcraft wallpapers gallery! The first part of the gallery features panoramas from in-game scenes. The second half contains screenshots. Hovering over the names of the images brings up more information. All panoramas are offered in 2 resolutions: full size and 2560x1440 (8:5 aspect, may be cropped).</p>
+
+	<p>Starting with the games release in February 2005 I played an undead mage named <a href="ava/241Storke_432576.png">Storke</a> on the German server Lothar. I was part of the fraction-leading <a href="screens/097ExpiredDeadline-Wallpaper.jpg">raid guild MC1</a> and later founder and guild leader of <a href="screens/306Evolution_1280720.jpg">Evolution</a> until <a href="ava/bye2.jpg">I quit</a> playing in the summer of 2007.</p>
+
+	<p>My interest in taking these in-game photos developed alongside my fascination to play the game. A big inspiration at the time were <a href="http://www.nebdaar.com/wow/">nebdaar's panoramas</a>. I started to get more involved with panoramas and picture manipulation. This is how this gallery developed.</p>
+	</div></div>
+</li>
+
+<li>Download
+	<div><div>
+	To batch download all images, save the following links:
+	<ul>
+		<li><a href="download.php?panos/16x10_2560_q90/">All panoramas, 2560x1440, 32MiB</a></li>
+		<li><a href="download.php?panos/FULL_q70/">All panoramas, full size, 41MiB</a></li>
+		<li><a href="download.php?screens/full/">All screenshots, 12MiB</a></li>
+	</ul>
+	</div></div>
+</li>
+
+<li>Licensing
+	<div><div>
+I am the original author of all images provided in this gallery. I did assemble them here for you to enjoy with much love and care. Feel free to share them, but please credit me by linking to this website. I license them under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution ShareAlike</a>.
+	</div></div>
+</li>
+
+<li>Honors
+	<div><div>
+		<p>This site has been honored by <i>Vaneck and Gnorog</i> on the 2006-03-10 german community watch and by <i>Drysc</i> on the 2008-04-02 US community spotlight. These set the start for the 
+		gallery being widely known and referred to on many, many other sites.</p>
+		<p>There have been many encouraging voices among all sorts of people. This would be too much to list here. Thank you all for your kind support!</p>
+	</div></div>
+</li>
+
+<li>Acknowledgments
+	<div><div>
+	Special thanks to
+	<a href="screens/061Incoming.jpg">Astaroth</a>
+	for hosting support of my first gallery,
+	<a href="screens/114_050.jpg">Arji</a>
+	for just being lovely,
+	<a href="ava/VolvoxAva.jpg">Volvox</a>
+	for being crazy - keep it real!,
+	<a href="screens/146_120.jpg">Mournblade</a>
+	for being the best guild grandpa ever!, and
+	<a href="ava/ArtemisiaAva.gif">Artemisia</a>
+	for granting me access to her account so that I could capture the last wallpapers! I'd also like to thank
+	<a href="ava/PhageAva.gif">Phage</a>,
+	<a href="ava/TuwongaAva.gif">Tuwonga</a>,
+	<a href="ava/VexationAva.gif">Vexation</a>,
+	<a href="ava/TarabraxAva.jpg">Tarabrax</a>,
+	<a href="ava/AxuteAva.jpg">Axute</a>,
+	<a href="ava/RuffAva.gif">Ruff</a>,
+	<a href="screens/171HammerOfFatalJustice_020.jpg">Helix</a>,
+	<a href="ava/MelosAva.jpg">Melos</a>,
+	<a href="screens/168OmarKarokk_020.jpg">Rykhan</a>,
+	<a href="screens/138DraconicDestruction_020.jpg">Rion</a>,
+	<a href="ava/GotchaAva.jpg">Gotcha</a>,
+	<a href="panos/16x10_2560_q90/295_290_16x10_2560_q90.jpg">Massel</a> and
+	<a href="panos/16x10_2560_q90/309_090_16x10_2560_q90.jpg">Fyor</a>
+	and many more for being such great models! :)
+	</div></div>
+</li>
+
+<li>Contact
+	<div><div>
+		m<b></b>@mgrf.de
+	</div></div>
+</li>
+</ul>
+</header>
+	
+<div style="clear: both;"></div>
+
+	
+	
+<main>
+	<section id="panoramas">
+
+<?php
+$panos = array(
+	array("318_060", 4414, 2278, "Golden Reign", 'Taken in Azshara.'),
+	array("317_032", 4810, 3006, "The Temple of The Moon"),
+	array("314_105", 6045, 1921, "Nagrand Outback"),
+	array("184_060", 4050, 2333, "Blackrock Mountain", 'The idea for this one was stolen from <a href="http://www.nebdaar.com/wow/wallpapers/wow-blackrock-mountain-1280x1024.jpg">nebdaar.com</a>. But mine is way better. :) Also, this is mirror-inverted.'),
+	array("230_141", 6054, 2939, "Brill", 'Brill got a new sky. :)'),
+	array("229_210", 4793, 2024, "Shattered Metro"),
+	array("185_150", 4575, 1828, "Darkshore"),
+	array("256_060", 3500, 2500, "Helms Bed Dale", 'One had to know a special way to get up there.'),
+	array("303_120", 5000, 4000, "O'ros"),
+	array("179_030", 3333, 1588, "Requiem"),
+	array("313_050", 7671, 4315, "The Crystal Hall"),
+	array("311_080", 6002, 3376, "Eco-Dome Midrealm"),
+	array("310_084", 7431, 3850, "Nightfall"),
+	array("295_290", 3510, 2194, "The Elvenpath", 'Massel. Made with passion while listening to <i>Nightwish - The Elvenpath</i>.'),
+	array("304_061", 4345, 2456, "Twisting Nether"),
+	array("302_080", 3385, 1586, "Gateway", 'This is actually a fake - a montage of 2 panoramas taken in Shattrath City.'),
+	array("300_120", 5500, 3438, "Court of the Sun"),
+	array("292_112", 4820, 3013, "Sen'Jin"),
+	array("283_173", 3828, 2071, "Altar of Storms"),
+	array("309_090", 3510, 2633, "Wings of a Dream", 'Fjor. I think he was the first 60 horde pala. :)'),
+	array("301_080", 3544, 2215, "Sweet Fang", 'Thank you Doomhawk!'),
+	array("316_060", 4650, 2541, "Terokkar Forest"),
+	array("315_080", 7699, 3021, "Sacred Tribunal"),
+	array("299_140", 2802, 2242, "Silvermoon City Inn")
+);
+
+foreach ($panos as $p) {
+echo "
+
+<article>
+	<a href='panos/16x10_2560_q90/".$p[0]."_16x10_2560_q90.jpg'>
+		<img src='panos/16x10_400_q70/".$p[0]."_16x10_400_q70.jpg'>
+		<h3>".$p[3]."</h3>
+	</a>
+	<div><div>".(array_key_exists(4, $p) ? $p[4]."<br/>" : "")."
+		<a href='panos/FULL_q70/".$p[0]."_full_q70.jpg'>".$p[1]." x ".$p[2]."</a>
+	</div></div>
+</article>";
+}
+?>
+
+</section>
+
+
+<section id="screenshots">
+	
+	<?php
+$screens = array(
+	array("298BlessDeath_020", "Bless Death", 'Tuwonga<br/>I made an <a href="ava/TuwongaAva.gif">animated avatar</a> in the same session.'),
+	array("297_164", "Beyond The Horizon", 'Lavagnome Raid: Paladin Grummelbart, Hunter Windforce, Warlock Roana, Warrior Gwoar, Rogue Zakna, Druid Massel, Priest Simon, Mage Tanja. The very best of the Allicance. I actually went up Gnomereagan with them to take this picture.'),
+	array("294_242", "Under The Favour Of Night", 'The great Sabot.'),
+	array("290_252", "Misfortunes Never Come Singly", 'Vexation, Tarabrax, Tibovete. Clearing Scholo with mages only was so much fun. :D'),
+	array("284_081", "Spearhead - The Welcome", 'Tibovete, Tarabrax, Storke, Zotty, Vexation.<br/> The undead elite mages. :)'),
+	array("282_141", "Dead Knight's Rite", 'Taken in Naxxramas. <i>Einherjer - Dead Knight´s Rite</i> is the namesake of this picture.'),
+	array("267_070", "Ras Frostwhisper"),
+	array("266_126", "The Icecold Trollblood", 'Portrait session with Phage. You may want to listen to <i>Finntroll - Det Iskalla Trollblod</i> to fully enjoy this picture. :)'),
+	array("228_090", "Agamand Mills"),
+	array("227_060", "Nemesis", 'Volvox in Shadowfang Keep. The heavier one: <i>Slipknot - Duality</i>. '),
+	array("212_078", "Daydream", 'Axute. <i>Nightwish - Slaying The Dreamer</i> best frames it.'),
+	array("175AquatisUr_020", "Aquatis Ur", 'Storke'),
+	array("171HammerOfFatalJustice_020", "Hammer Of Fatal Justice", 'Helix'),
+	array("170MissTanjaArcane_020", "Miss Tanja Arcane", 'Tanja. This was actually not arranged.'),
+	array("169Naquadria_020", "Naquadria", 'Nefarian in human form'),
+	array("168OmarKarokk_020", "Omar Karokk", 'Ryhkan'),
+	array("146_120", "Taste Of Hate", 'Mournblade'),
+	array("138DraconicDestruction_020", "Draconic Destruction", 'Rion'),
+	array("130EnsignOfTheBurningAnvil_020", "Ensign Of The Burning Anvil", 'Rykhan, Mogli durring a raid in Blackwing Lair.'),
+	array("117_170", "Fated", 'Storke'),
+	array("133Chillthrill_020", "Chillthrill"),
+	array("114_050", "When The Iron Tears The Heart", 'Arji. Looks better while listening to <i>Ensiferum - Iron</i>. ;)'),
+	array("113MoonlightMermaid_020", "Moonlight Mermaid", 'Storke'),
+	array("100OneHundred_020", "One Hundred", 'MC1 Raid'),
+	array("091RiseWithEnduringBattle_020", "Rise With Enduring Battle", 'MC1 Raid'),
+	array("076FromCradleToCoffin_020", "From Cradle To Coffin"),
+	array("066Insomnia_020", "Insomnia", 'Drunken in the kings throne room. Ohoh.'),
+	array("055ArcaneGravity_020", "Arcane Gravity", 'MC1 Raid'),
+	array("109_120", "Ice Queen", 'Another self portait. Listen to <i>Within Temptation - Ice Queen</i> to get the feeling. (I was a passionate frost mage.) Fabulous. :)'),
+	array("069Sandfury", "Sandfury"),
+	array("061Incoming", "Incoming", 'Astaroth'),
+	array("045TokenOfTime", "Token Of Time"),
+	array("038SpiritualHarmony_020", "Spiritual Harmony", 'Thoruz'),
+	array("036ImmaculateShellblastOfTheArcanist", "Shellblast of the Arcanist"),
+	array("024Cinderella_020", "Cinderella", 'Storke'),
+	array("004ADropInTheOcean_020", "A Drop In The Ocean", 'MC1 Raid in Molten Core - hence the name MC1. July 2005.')
+);
+
+foreach ($screens as $p) {
+echo "
+
+<article>
+	<a href='screens/full/".$p[0].".jpg'>
+		<img src='screens/300x225/".$p[0]."_300.jpg'>
+		<h3>".$p[1]."</h3>
+	</a>
+	".(array_key_exists(2, $p) ? "<div><div>".$p[2]."</div></div>" : "")."
+</article>";
+}
+?>
+
+
+</section>
+	
+
+</main>
+
+</body>
+</html>
